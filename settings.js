cfdfc25e33f48071d0c0e1a124e4fb06c705e390
pablo911 <142254758+Kakashixlol@users.noreply.github.com>
2024-08-14 02:11:11 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['201025663589']
+global.botname = '،mmmmm'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "mm Official"
+global.sticker2 = "🌜"
